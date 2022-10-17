891675bd3a8a66018f0d1ac40fea324534f1b018
Nikolinc <tsigulskynikita@gmail.com>
2022-10-17 19:58:38 +0500

    add window user

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { AppBar, Toolbar } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+import { Grid, Avatar, Button, AppBar, Toolbar } from "@mui/material/";
 import { LOGIN_ROUTES } from "../utils/const";
 import { Context } from "../index";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -13,28 +11,48 @@ const Navbar = () => {
 
   return (
     <AppBar position="static" style={{ background: "rgb(54, 54, 54)" }}>
-      <Toolbar variant={"dense"}>
-        <Grid
-          ccontainer
-          direction={"row-reverse"}
-          justify={"flex-end"}
-          alignItems={"center"}
-        >
-          {" "}
+      <Toolbar variant={"dense"} sx={{ flexGrow: 2 }}>
+        <Grid container spacing={2} minHeight={20}>
           {user ? (
-            <Button
-              onClick={() => auth.signOut()}
-              variant="outlined"
-              style={{ color: "lightgray" }}
-            >
-              Go out
-            </Button>
+            <>
+              <Grid
+                xs
+                display="flex"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                style={{ marginleft: "50px" }}
+              >
+                <Avatar src={user.photoURL} alt={user.displayName} />
+              </Grid>
+              <Grid
+                xs
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+                style={{ marginRight: "50px" }}
+              >
+                <Button
+                  onClick={() => auth.signOut()}
+                  variant="outlined"
+                  style={{ color: "lightgray" }}
+                >
+                  Go out
+                </Button>
+              </Grid>
+            </>
           ) : (
-            <NavLink to={LOGIN_ROUTES}>
-              <Button variant="outlined" style={{ color: "lightgray" }}>
-                Login
-              </Button>
-            </NavLink>
+            <Grid
+              xs
+              display="flex"
+              justifyContent="space-between"
+              alignItems="flex-end"
+            >
+              <NavLink to={LOGIN_ROUTES}>
+                <Button variant="outlined" style={{ color: "lightgray" }}>
+                  Login
+                </Button>
+              </NavLink>
+            </Grid>
           )}
         </Grid>
       </Toolbar>
