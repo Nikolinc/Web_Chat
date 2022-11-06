ee28bf162c510027075824b2c890cd469312b576
Nikolinc <tsigulskynikita@gmail.com>
2022-11-06 21:16:04 +0500

    add error page

---
+++ src/components/noPages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.scss";
+
+export default function NoPages() {
+  return (
+    <div className="noPage">
+      <h1>This page is currently under development</h1>
+    </div>
+  );
+}
