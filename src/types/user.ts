2bbcf130972ddb002cb66b066ce25982dab2290e
Nikolinc <tsigulskynikita@gmail.com>
2022-11-06 21:11:07 +0500

    add type user

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,28 +1,28 @@
-export interface UserState{
-  users:any[],
-  loading:boolean,
-  error:null|string,
-  
+export interface UserState {
+  users: any;
+  loading: boolean;
+  error: null | string;
 }
 
-export enum userActionTypes{
-   FETCH_USER = "FETCH_USER",
-   FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS",
-   FETCH_USER_ERROR = "FETCH_USER_ERROR",
+export enum userActionTypes {
+  FETCH_USER = "FETCH_USER",
+  FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS",
+  FETCH_USER_ERROR = "FETCH_USER_ERROR",
 }
 
-interface FetnUserAction{
-  type:userActionTypes.FETCH_USER;
+interface FetnUserAction {
+  type: userActionTypes.FETCH_USER;
 }
-interface FetnUserSuccessAction{
-  type:userActionTypes.FETCH_USER_SUCCESS;
-  payload:any[]
+interface FetnUserSuccessAction {
+  type: userActionTypes.FETCH_USER_SUCCESS;
+  payload: any;
 }
-interface FetnUserErrorAction{
-  type:userActionTypes.FETCH_USER_ERROR;
-  payload:string;
+interface FetnUserErrorAction {
+  type: userActionTypes.FETCH_USER_ERROR;
+  payload: string;
 }
 
-export type userAction = FetnUserAction|FetnUserSuccessAction|FetnUserErrorAction;
-
-
+export type userAction =
+  | FetnUserAction
+  | FetnUserSuccessAction
+  | FetnUserErrorAction;
